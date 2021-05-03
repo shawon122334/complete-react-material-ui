bd2dbffc8d3f4d7870d40db3c4bf2d8d361e161a
shawon122334 <shawon.mu11@gmail.com>
2021-05-03 23:22:11 +0600

    working on side menu

---
+++ src/App/App.js
@@ -0,0 +1,13 @@
+import SideMenu from '../components/SideMenu';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <SideMenu></SideMenu>
+      <div>here we go</div>
+    </div>
+  );
+}
+
+export default App;
