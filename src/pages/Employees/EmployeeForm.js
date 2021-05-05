78d5cc7832fe784273fca619be08f5d0ccce45c6
shawon122334 <shawon.mu11@gmail.com>
2021-05-05 17:22:48 +0600

    working on form

--- src/pages/Employees/EmployeeForm.js
+++ src/pages/Employees/EmployeeForm.js
@@ -1,5 +1,17 @@
-import { Grid, TextField } from '@material-ui/core';
+import { Grid, makeStyles, TextField } from '@material-ui/core';
+import { ClassSharp } from '@material-ui/icons';
 import React, { useState } from 'react'
+
+//useStyle is to add custom css style
+const useStyle= makeStyles(theme=>({
+    root:{
+        '& .MuiFormControl-root ':{
+            width:'80%',
+            margin: theme.spacing(1)
+        }
+    }
+}))
+
 const initialFValues={
     id:0,
     fullName:'',
@@ -13,12 +25,16 @@ const initialFValues={
 }
 export default function EmployeeForm() {
     const [values,setValues] = useState(initialFValues);
+    const classes = useStyle();
 
+    const handleInputchange=e=>{
+        const {name,value}= e.target;
+    }
     return (
-        <form>
+        <form className={classes.root}>
             <Grid container>
                 <Grid item xs={6}>
-                    <TextField variant="outlined" label="Full Name" value={values.fullName}/>
+                    <TextField variant="outlined" label="Full Name" name="fullName" value={values.fullName} onChange={handleInputchange}/>
                     <TextField variant="outlined" label="Email" value={values.email}/>
                 </Grid>
                 <Grid item xs={6}>
--- src/pages/Employees/Employees.js
+++ src/pages/Employees/Employees.js
@@ -2,12 +2,21 @@ import React from 'react'
 import PageHeader from '../../components/PageHeader'
 import EmployeeForm from './EmployeeForm'
 import PeopleOutlineIcon from '@material-ui/icons/PeopleOutline';
+import { makeStyles, Paper } from '@material-ui/core';
+
+const useStyles= makeStyles(theme=>({
+    pageContent:{
+        margin: theme.spacing(5),
+        padding: theme.spacing(3)
+    }
+}))
 
 export default function Employees() {
+    const classes= useStyles();
     return (
         <div>
             <PageHeader title="New Employee" subTitle="Form Design With Validations" icon={<PeopleOutlineIcon fontSize="large"/>}/>
-            <EmployeeForm/>
+           <Paper className={classes.pageContent}> <EmployeeForm/> </Paper> 
         </div>
     )
 }
