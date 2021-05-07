b513d15319ffadcc868577fbc266f9be915d1448
shawon122334 <shawon.mu11@gmail.com>
2021-05-07 23:58:19 +0600

    gender added

--- src/pages/Employees/EmployeeForm.js
+++ src/pages/Employees/EmployeeForm.js
@@ -34,7 +34,7 @@ export default function EmployeeForm() {
                         <RadioGroup row={true} name="gender" value={values.gender} onChange={handleInputChange}>
                             <FormControlLabel value="male" control={ <Radio/>} label="Male" />
                             <FormControlLabel value="female" control={<Radio />} label="Female" />
-                            
+                            <FormControlLabel value="other" control={<Radio />} label="Other" />
                         </RadioGroup>
                     </FormControl>
                 </Grid>
